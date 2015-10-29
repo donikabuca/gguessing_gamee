35a604fbca6ac40f01d11de069ff3d499c074749
Donika Buca <donche91@hotmail.com>
2015-10-29 01:58:03 -0400

    This is the fisr commit

---
+++ guessgame2.js
@@ -0,0 +1,36 @@
+
+var score = 0
+var birthplace = prompt('Was i born in Albania? Yes or No');
+     if (birthplace === 'yes') {
+     	alert('Yeahhh you are correct');
+ } else {
+     	alert('sorry you are wrong try next time');
+ }
+var score = 1
+     
+var color = prompt('is my favorite color blue? Yes or No');
+    if (color === 'yes') {
+     alert('right');
+    } else {
+  
+    alert('You are wrong');  
+}
+  var score = 1
+
+var car = prompt('is my favorite car BMW?Yes or NO');
+    if (car === 'yes') {
+     	alert('correct')
+  } else {
+     	alert('Wrong')
+  }
+
+var phone = prompt('Do i use Iphone?Yes or No');
+    if (Iphone === 'yes') {
+    	alert('correct');
+ } else {
+    	alert('no you are wrong I use Iphone')
+ }
+   
+
+
+
---
+++ java.sc
@@ -0,0 +1,8 @@
+<!Docutype html>
+<html>
+<head>
+<title>Guessgame</title>
+<script>
+   var score = 0;
+   alert("Hello!Let's play guessing game");
+   
